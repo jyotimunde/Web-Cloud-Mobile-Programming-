6bc8f4df16d0ffb6dbb4c1a000b3f6d4074ebbc6
jyotimunde <37389860+jyotimunde@users.noreply.github.com>
2018-11-01 00:09:52 -0500

    changes

---
+++ Assignment 3/twitter/Souce/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key:         'bKS2EqJSKIhQrRNKuv1R95HpT',
+    consumer_secret:      'HhLIoQMQLoTVWPjxsx2u4iViP5ibH7OpCvIalQAioG9hC56NXe',
+    access_token:         '3273633594-PJCncpqYxAbvmjpW6jest3HejYi6j6JLYQIA3hK',
+    access_token_secret:  'EFrjMAiOr1DyDurcnvkhnLZrN51XTkOiynyXNAHxOaoxx'
+}
